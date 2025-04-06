51282645c43d61bef336f4984b7cedfe801841d8
davetony007 <ayeyedough@gmail.com>
2025-04-06 21:47:46 +0100

    Add Google site verification meta tag to head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,7 @@ export default function Head() {
         name="description"
         content="Explore free online tools for text conversion, URL encoding, color conversion, JSON formatting, and password generation."
       />
+      <meta name="google-site-verification" content="e22Me2xmW74B2UEGaXRO-y9IzQy0t3NA8Dcd2Ub7DTw" />
     </>
   );
 }
